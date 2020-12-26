369a7922277c423a779fe4018a88c3657d79c29c
Vaibhav Dixit <43854458+vaibdix@users.noreply.github.com>
2020-12-26 23:48:54 +0530

    Add files via upload

---
+++ Management System/audiopreview.php
@@ -0,0 +1,193 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+
+
+<style>
+	
+p{
+margin-left: 200px;
+margin-right: 200px;
+}
+
+span{
+margin-left: 150px;
+margin-right: 150px;
+}
+
+
+</style>
+
+</head>
+<body>
+<?php include('includes/header.php');?>
+<div class="banner">
+	<div class="container">
+		
+	</div>
+</div>
+
+
+<p align="right" padding=90px >
+<br><br>
+
+
+<audio controls  >
+<source src="ringtones/crusade-mm.mp3" type="audio/mp3">
+</audio>
+&nbsp;&nbsp;&nbsp;&nbsp;<font size=6.5 color="#6ef69">Artist--00.mp3</font>
+<br><br>
+</p>
+
+<span align="left" padding=90px>
+<font size=6.5 color="#6ef69">Artist--01.mp3 &nbsp;&nbsp;&nbsp;&nbsp;</font>
+<audio controls>
+<source src="ringtones/TongueTied-mm.mp3" type="audio/mp3">
+</audio>
+
+<br><br>
+</span>
+
+<p align="right" padding=90px >
+<audio controls>
+<source src="ringtones/alone-mm.mp3" type="audio/mp3">
+</audio>
+&nbsp;&nbsp;&nbsp;&nbsp;<font size=6.5 color="#6ef69">Artist--02.mp3</font>
+<br><br>
+</p>
+
+<span align="left" padding=90px>
+<font size=6.5 color="#6ef69">Artist--03.mp3&nbsp;&nbsp;&nbsp;&nbsp;</font>
+<audio controls>
+<source src="ringtones/holdon-mg.mp3" type="audio/mp3">
+</audio>
+<br><br>
+</span>
+
+
+<p align="right" padding=90px >
+<audio controls>
+<source src="ringtones/usedtolove-mg.mp3" type="audio/mp3">
+</audio>
+&nbsp;&nbsp;&nbsp;&nbsp;<font size=6.5 color="#6ef69">Artist--04.mp3</font>
+<br><br>
+</p>
+
+
+<span align="left" padding=90px>
+<font size=6.5 color="#6ef69">Artist--05.mp3&nbsp;&nbsp;&nbsp;&nbsp;</font>
+<audio controls>
+<source src="ringtones/drown-mg.mp3" type="audio/mp3">
+</audio>
+<br><br>
+</span>
+
+
+<p align="right" padding=90px >
+<audio controls>
+<source src="ringtones/TakeYouDown-ill.mp3" type="audio/mp3">
+</audio>
+&nbsp;&nbsp;&nbsp;&nbsp;<font size=6.5 color="#6ef69">Artist--06.mp3</font>
+<br><br>
+</p>
+
+
+<span align="left" padding=90px>
+<font size=6.5 color="#6ef69">Artist--07.mp3&nbsp;&nbsp;&nbsp;&nbsp;</font>
+<audio controls>
+<source src="ringtones/holdon-ill.mp3" type="audio/mp3">
+</audio>
+<br><br>
+</span>
+
+
+
+<p align="right" padding=90px >
+<audio controls>
+<source src="ringtones/cheese-deadmau5.mp3" type="audio/mp3">
+</audio>
+&nbsp;&nbsp;&nbsp;&nbsp;<font size=6.5 color="#6ef69">Artist--08.mp3</font>
+<br><br>
+</p>
+
+
+<span align="left" padding=90px>
+<font size=6.5 color="#6ef69">Artist--09.mp3&nbsp;&nbsp;&nbsp;&nbsp;</font>
+<audio controls>
+<source src="ringtones/monophobia-deadmau5.mp3" type="audio/mp3">
+</audio>
+<br><br>
+</span>
+
+
+<p align="right" padding=90px >
+<audio controls>
+<source src="ringtones/Glivch-deadmau5.mp3" type="audio/mp3">
+</audio>
+&nbsp;&nbsp;&nbsp;&nbsp;<font size=6.5 color="#6ef69">Artist--10.mp3</font>
+<br><br>
+</p>
+
+
+<span align="left" padding=90px>
+<font size=6.5 color="#6ef69">Artist--11.mp3&nbsp;&nbsp;&nbsp;&nbsp;</font>
+<audio controls>
+<source src="ringtones/dobadwell-kshmr.mp3" type="audio/mp3">
+</audio>
+<br><br>
+</span>
+
+
+<p align="right" padding=90px >
+<audio controls>
+<source src="ringtones/noregreats-kshmr.mp3" type="audio/mp3">
+</audio>
+&nbsp;&nbsp;&nbsp;&nbsp;<font size=6.5 color="#6ef69">Artist--12.mp3</font>
+<br><br>
+</p>
+
+
+
+
+
+
+
+
+
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>			
+<!-- //write us -->
+</body>
+</html>
---
+++ Management System/index.php
@@ -0,0 +1,103 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Music Management System</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+
+</head>
+<body>
+<?php include('includes/header.php');?>
+<div class="banner">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> Music Management System</h1>
+	</div>
+</div>
+
+
+
+
+
+<!---holiday---->
+<div class="container">
+	<div class="holiday">
+	
+
+
+
+	
+	<h3><font color = #f3e5f5>Upcoming Events</font></h3>
+<br>
+					
+<?php $sql = "SELECT * from tbltourpackages order by rand() limit 4";
+$query = $dbh->prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{	?>
+			<div class="rom-btm">
+				<div class="col-md-3 room-left wow fadeInLeft animated" data-wow-delay=".5s">
+					<img src="admin/pacakgeimages/<?php echo htmlentities($result->PackageImage);?>" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-6 room-midle wow fadeInUp animated" data-wow-delay=".5s">
+					<h4><font color = rgb(255,0,0)>Artist Name: <?php echo htmlentities($result->PackageName);?></font></h4>
+					<h6><font color = #d3dd18 background color:#fff;> Package Type : <?php echo htmlentities($result->PackageType);?></font></h6>
+					<p><b>Event Location :</b> <?php echo htmlentities($result->PackageLocation);?></p>
+					<p><b>Features</b> <?php echo htmlentities($result->PackageFetures);?></p>
+				</div>
+				<div class="col-md-3 room-right wow fadeInRight animated" data-wow-delay=".5s">
+					<h5>INR <?php echo htmlentities($result->PackagePrice);?></h5>
+					<a href="package-details.php?pkgid=<?php echo htmlentities($result->PackageId);?>" class="view"><font color = #C0FF00>Details</font></a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+
+<?php }} ?>
+			
+		
+<div><a href="package-list.php" class="view">View More Packages</a></div>
+</div>
+			<div class="clearfix"></div>
+	</div>
+
+
+
+
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>			
+<!-- //write us -->
+</body>
+</html>
