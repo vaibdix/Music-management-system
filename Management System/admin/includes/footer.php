3035a6cb2eef4734f862dae4825c7857f0022416
Vaibhav Dixit <43854458+vaibdix@users.noreply.github.com>
2020-12-26 23:36:16 +0530

    Add files via upload

---
+++ Management System/admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copyrights">
+	 <p>© 2016 MMS. All Rights Reserved |  <a href="#">MMS</a> </p>
+</div>	
---
+++ Management System/admin/includes/sidebarmenu.php
@@ -0,0 +1,23 @@
+<div class="sidebar-menu">
+					<header class="logo1">
+						<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> 
+					</header>
+						<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
+                           <div class="menu">
+									<ul id="menu" >
+										<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span><div class="clearfix"></div></a></li>
+										
+									 <li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span> Tour Packages</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+										   <ul id="menu-academico-sub" >
+										   <li id="menu-academico-avaliacoes" ><a href="create-package.php">Create</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">Manage</a></li>
+
+										  </ul>
+										</li>
+									<li id="menu-academico" ><a href="manage-users.php"><i class="fa fa-users" aria-hidden="true"></i><span>Manage Users</span><div class="clearfix"></div></a></li>
+									<li id="menu-academico-avaliacoes" ><a href="http://127.0.0.1:8091/scriptcase/app/report/chart_tblbooking/?nmgp_outra_jan=true&nmgp_start=SC&script_case_session=5b5e55a4451325f9f63c7721c8db58b3&550">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MMS Report</a></li>
+									<li id="menu-academico-avaliacoes" ><a href="http://127.0.0.1:8091/scriptcase/app/shopping/chart_orders/?nmgp_outra_jan=true&nmgp_start=SC&script_case_session=5b5e55a4451325f9f63c7721c8db58b3&2848">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Shopping Report</a></li>
+									
+								  </ul>
+								</div>
+							  </div>
